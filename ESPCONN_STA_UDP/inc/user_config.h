9fcc2f8a27280c54e8e5f6d8fb817af017d32b87
cnuptak <cfkzvbcfkzvb@gmail.com>
2020-09-25 21:57:28 +0300

    Update user_config.h

--- ESPCONN_STA_UDP/inc/user_config.h
+++ ESPCONN_STA_UDP/inc/user_config.h
@@ -1,8 +1,8 @@
 #ifndef __USER_CONFIG_H__
 #define __USER_CONFIG_H__
 //------------------------------------------------------
-#define WIFI_CLIENTSSID "RT-WiFi_DE8F"
-#define WIFI_CLIENTPASSWORD "03072020"
+#define WIFI_CLIENTSSID ""
+#define WIFI_CLIENTPASSWORD ""
 #define UDPSERVERIP "192.168.0.13"
 #define UDPSERVERPORT 30000
 //------------------------------------------------------
