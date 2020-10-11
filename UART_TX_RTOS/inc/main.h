6133906462a7a6aac097f33382570ecdebcce255
cnuptak <cfkzvbcfkzvb@gmail.com>
2020-10-11 11:35:07 +0300

    + freeRTOS

---
+++ UART_TX_RTOS/inc/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+//------------------------------------------------
+#include "esp_common.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "uart.h"
+#include "gpio.h"
+//------------------------------------------------
+#endif /* MAIN_H_ */
